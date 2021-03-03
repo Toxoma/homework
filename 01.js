af0462c053de539ef52a246ef9e0218f64c1448f
Toxoma <74244286+Toxoma@users.noreply.github.com>
2021-03-03 20:00:16 +0300

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,13 @@
+function number(num) {
+    switch (num) {
+        case 5: return 'пять'
+        case 13: return 'тринадцать'
+        case 22: return 'двадцать два'
+        case 35: return 'тридцать пять'
+        case 98: return 'девяносто восемь'
+        default: return num
+    }
+}
+
+let x = 5 //? ваше число
+console.log(number(x))
